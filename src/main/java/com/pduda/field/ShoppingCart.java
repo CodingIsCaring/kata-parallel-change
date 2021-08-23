00a784e422500e3ce653dca85735b655b3e281ee
yodralopez <yodralopez@gmail.com>
2021-08-23 19:05:08 +0100

    parallel change: shoppingCart with list of items

--- src/main/java/com/pduda/field/ShoppingCart.java
+++ src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,21 +1,33 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
-    private int price;
+    private List<Integer> prices = new ArrayList<>();
 
     public void add(int price) {
-        this.price = price;
+        this.prices.add(price);
     }
 
     public int calculateTotalPrice() {
-        return price;
+        Integer total = 0;
+        for (int price : this.prices) {
+            total += price;
+        }
+        return total;
     }
 
     public boolean hasDiscount() {
-        return price >= 100;
+        for (int price : this.prices) {
+            if (price >= 100) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int numberOfProducts() {
-        return 1;
+        return prices.size();
     }
 }
--- src/test/java/com/pduda/field/ShoppingCartTest.java
+++ src/test/java/com/pduda/field/ShoppingCartTest.java
@@ -1,41 +1,45 @@
 package com.pduda.field;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ShoppingCartTest {
 
-
     @Test
-    public void singleItem_numberOfProductsInTheCart() throws Exception {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.add(10);
-
-        Assert.assertEquals(1, shoppingCart.numberOfProducts());
-    }
-
-    @Test
-    public void singleItem_totalPrice() throws Exception {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.add(10);
-
-        Assert.assertEquals(10, shoppingCart.calculateTotalPrice());
-    }
-
-    @Test
-    public void singleItem_hasDiscountIfContainsAtLeastOneProductWorthAtLeast100() throws Exception {
+    public void should_count_number_of_products_in_the_cart() {
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.add(100);
+        shoppingCart.add(10);
 
-        Assert.assertTrue(shoppingCart.hasDiscount());
+        assertEquals(2, shoppingCart.numberOfProducts());
     }
 
     @Test
-    public void singleItem_doesNotHaveDiscountIfContainsNoProductsWorthAtLeast100() throws Exception {
+    public void should_calculate_total_price_of_cart() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.add(10);
+        shoppingCart.add(10);
+
+        assertEquals(20, shoppingCart.calculateTotalPrice());
+    }
+
+    @Test
+    public void should_have_discount_if_contains_at_least_one_product_worth_at_least_100() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.add(100);
+        shoppingCart.add(10);
+
+        assertTrue(shoppingCart.hasDiscount());
+    }
+
+    @Test
+    public void should_not_have_discount_if_contains_no_products_worth_at_least_100() {
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.add(99);
+        shoppingCart.add(10);
 
-        Assert.assertFalse(shoppingCart.hasDiscount());
+        assertFalse(shoppingCart.hasDiscount());
     }
 
 }
